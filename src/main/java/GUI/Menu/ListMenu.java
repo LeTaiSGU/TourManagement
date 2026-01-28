322fcddce058393b890e76706407b30436cc7861
LeTaiSGU <letaikun11112@gmail.com>
2026-01-28 13:10:12 +0700

    push main menu GUI

---
+++ src/main/java/GUI/Menu/ListMenu.java
@@ -0,0 +1,102 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI.Menu;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
+import java.util.List;
+import javax.swing.SwingUtilities;
+/**
+ *
+ * @author letan
+ */
+public class ListMenu<E extends Object> extends JList<E> {
+    
+    public void addEventSelectedMenu(EventMenuSelected event){
+        events.add(event);
+    }
+    private final DefaultListModel model;
+    private final List<EventMenuSelected> events;
+    private int selectedIndex = -1;
+    
+    public ListMenu() {
+        model = new DefaultListModel();
+        events = new ArrayList<>();
+        super.setModel(model);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                if (SwingUtilities.isLeftMouseButton(me)) {
+                    int index = locationToIndex(me.getPoint());
+                    Object obj = model.getElementAt(index);
+                    if (obj instanceof Model_Menu) {
+                        Model_Menu data = (Model_Menu) obj;
+                        if (data.getType() == Model_Menu.MenuType.MENU) {
+                            if (index != selectedIndex) {
+                                selectedIndex = -1;
+                                runEvent(index);
+                            }
+                        }
+                    } else {
+                        if (index != selectedIndex) {
+                            selectedIndex = -1;
+                            runEvent(index);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    private void runEvent(int indexChange) {
+        for (EventMenuSelected event : events) {
+            event.menuSelected(indexChange, new EventMenuCallBack() {
+                @Override
+                public void call(int index) {
+                    //  This call back event run when animation done
+                    selectedIndex = index;
+                    repaint();
+                }
+            });
+        }
+    }
+    @Override
+    public ListCellRenderer<? super E> getCellRenderer() {
+        return new DefaultListCellRenderer(){
+            @Override
+            public Component getListCellRendererComponent(JList<?> jlist, Object o, int index, boolean selected, boolean focus) {
+                Model_Menu data;
+                if (o instanceof Model_Menu) {
+                    data = (Model_Menu) o;
+                } else {
+                    data = new Model_Menu("7", o + "", Model_Menu.MenuType.MENU);
+                }
+                MenuItem item = new MenuItem(data);
+                item.setSelected(index == selectedIndex);
+                return item;
+            }
+
+        };
+    }
+    
+    @Override
+    public void setModel(ListModel<E> lm) {
+        for (int i = 0; i < lm.getSize(); i++) {
+            model.addElement(lm.getElementAt(i));
+        }
+    }
+
+    public void addItem(Model_Menu data) {
+        model.addElement(data);
+    }
+
+}
---
+++ src/main/java/GUI/Menu/MenuItem.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package GUI.Menu;
+
+import java.awt.Color;
+import javax.swing.BorderFactory;
+
+/**
+ *
+ * @author letan
+ */
+public class MenuItem extends javax.swing.JPanel {
+
+    private final Model_Menu data;
+    
+    public MenuItem(Model_Menu data) {
+        this.data = data;
+        initComponents();
+        setOpaque(false);
+        
+        if (data.getType() == Model_Menu.MenuType.MENU) {
+            lbIcon.setIcon(data.toIcon());
+            lbName.setText(data.getName());
+        } else {
+            lbName.setText(" ");
+        }
+    }
+
+    public void setSelected(boolean selected) {
+        if (data.getType() == Model_Menu.MenuType.MENU) {
+            if (selected) {
+                lbIcon.setIcon(data.toIconSelected());
+                lbName.setForeground(Color.decode("#6DD5FA"));
+            } else {
+                lbIcon.setIcon(data.toIcon());
+                lbName.setForeground(new Color(255, 255, 255));
+            }
+        }
+    }
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbIcon = new javax.swing.JLabel();
+        lbName = new javax.swing.JLabel();
+
+        setPreferredSize(new java.awt.Dimension(123, 50));
+
+        lbName.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lbName.setForeground(new java.awt.Color(226, 226, 226));
+        lbName.setText("Menu Item");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lbIcon)
+                .addGap(18, 18, 18)
+                .addComponent(lbName, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lbIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(lbName, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lbIcon;
+    private javax.swing.JLabel lbName;
+    // End of variables declaration//GEN-END:variables
+}
